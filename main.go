6a2c336cb4163f8956777f6de223f1907f37fecf
noisetech <box92143@gmail.com>
2024-09-06 19:57:20 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bwa-startup/user"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/bwa-startup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+
+	user := user.User{
+		Name: "Test Simpan",
+	}
+
+	userRepository.Save(user)
+}
